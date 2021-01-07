c7a42df982daeb0756ead5751a31d0ba18d838f6
tiansial <bomdia@tiansial.com>
2021-01-07 16:49:02 +0000

    Create pt-PT.js

---
+++ packages/theme-docs/src/i18n/pt-PT.js
@@ -0,0 +1,11 @@
+export default {
+  search: {
+    placeholder: 'Pesquise os documentos (pressione "/" para focar)'
+  },
+  toc: {
+    title: 'Nesta página'
+  },
+  article: {
+    github: 'Editar esta página no GitHub'
+  }
+}
